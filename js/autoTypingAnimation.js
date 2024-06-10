5edfaf9dad18e863f72fc94c666568743db56eea
ADITI SHARMA <165453733+imaditisharmaa@users.noreply.github.com>
2024-06-10 15:43:37 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>I'm currently pursuing my final year Bachelor's Degree in Biomedical Engineering with specialization in AI & ML.My expertise in the areas of Biomedical Engineering allows me to approach projects with confidence and deliver high-quality results.I strongly believe in the value of continuous learning and staying adaptable in the ever-evolving field of Healthcare.I actively seek opportunities to expand my knowledge and skills, embracing new technologies and innovative approaches.Feel free to explore my work and reach out if you have any questions or opportunities for collaboration."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
