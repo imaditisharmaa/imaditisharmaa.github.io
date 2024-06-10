21e52072870fd6322ca4f09cbb5bc13c778d6de0
ADITI SHARMA <165453733+imaditisharmaa@users.noreply.github.com>
2024-06-11 00:25:40 +0530

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Aditi Sharmaa", "BioMedical Engineer"];
+const textArray = ["Aditi Sharmaa", "Aditi Sharmaa"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
