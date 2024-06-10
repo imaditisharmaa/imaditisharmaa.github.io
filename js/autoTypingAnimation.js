12a85af32db866e229e8e1a7d20e4f17e9300b0a
ADITI SHARMA <165453733+imaditisharmaa@users.noreply.github.com>
2024-06-10 16:13:16 +0530

    Update autoTypingAnimation.js

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>I'm currently pursuing my final year Bachelor's Degree in Biomedical Engineering with specialization in AI & ML.My expertise in the areas of Biomedical Engineering allows me to approach projects with confidence and deliver high-quality results.I strongly believe in the value of continuous learning and staying adaptable in the ever-evolving field of Healthcare.I actively seek opportunities to expand my knowledge and skills, embracing new technologies and innovative approaches.Feel free to explore my work and reach out if you have any questions or opportunities for collaboration."
+        "<span>Biomedical Engineer:My expertise in the areas of Biomedical Engineering allows me to approach projects with confidence and deliver high-quality results.I strongly believe in the value of continuous learning and staying adaptable in the ever-evolving field of Healthcare.I actively seek opportunities to expand my knowledge and skills, embracing new technologies and innovative approaches.."
       ],
       typeSpeed: 7,
       contentType: 'html'
